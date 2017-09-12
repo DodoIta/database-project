50dd715b5a5635d721032733c302eca884ec602c
DodoIta <davide.cor94@gmail.com>
2017-09-12 19:18:30 +0200

    completata registrazione

--- interfaccia/php/registrazione.php
+++ interfaccia/php/registrazione.php
@@ -13,13 +13,24 @@ if(!isset($_POST['nome']) || $_POST['nome']=="")
           echo "Inserire un'e-mail valida.";
         else {
           include "metodi.php";
+          $database = new db_methods();
           // recupero i valori dei dati inseriti nel form
           $nome = $_POST['nome'];
           $cognome = $_POST['cognome'];
           $email = $_POST['email'];
-          $password = $_POST['pass'];
+          $password = $_POST['password'];
           $sesso = $_POST['sesso'];
-
-
+          $occupazione = $_POST['occupazione'];
+          // mi connetto al db
+          $database->connetti();
+          // acquisisco il relativo lavoro
+          $temp = $database->query("SELECT id_lavoro FROM lavoro WHERE nome_lavoro = '$occupazione'");
+          $id_lavoro = $database->estrai($temp);
+          // inserisco il nuovo utente
+          $query = "INSERT INTO utente
+                    VALUES (2, '$nome', '$cognome', '$sesso', '$email', '$password',
+                            '1994-03-10', NULL, '$id_lavoro', NULL, NULL, 'free')";
+          // disconnessione
+          $database->disconnetti();
         }
 ?>
